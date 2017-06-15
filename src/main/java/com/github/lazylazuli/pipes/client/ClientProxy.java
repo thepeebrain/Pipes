2a1fc95a41edaacedf8f4a9bbcdef08c4d3298a7
LazyLazuli <phoebeang@icloud.com>
2017-06-16 00:51:16 +0800

    fix item models not showing

--- src/main/java/com/github/lazylazuli/pipes/client/ClientProxy.java
+++ src/main/java/com/github/lazylazuli/pipes/client/ClientProxy.java
@@ -1,5 +1,18 @@
 package com.github.lazylazuli.pipes.client;
 
 import com.github.lazylazuli.pipes.common.CommonProxy;
+import com.github.lazylazuli.pipes.common.PipeObjects;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-public class ClientProxy extends CommonProxy {}
+public class ClientProxy extends CommonProxy
+{
+	
+	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		super.preInit(event);
+		
+		setModelResource(PipeObjects.PIPE);
+		setModelResource(PipeObjects.PIPE_WINDOWED);
+	}
+}
